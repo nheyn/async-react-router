dd56ad0877a3669092c5fdb9dc6883cea72bb5bd
nheyn <nick@heyn.io>
2015-05-07 17:13:35 -0600

    Add Mixin in For React Router Handlers
    Added a mixin that can be used to get the results of each
    ‘getAsyncInitialState’ in a react-router route.

---
+++ src/asyncInitialStateHandlerMixin.js
@@ -0,0 +1,37 @@
+/**
+ * @flow
+ */
+var React = require('react');
+var { RouteHandler } = require('react-router');
+
+/*------------------------------------------------------------------------------------------------*/
+//	--- Async Initial State Mixin ---
+/*------------------------------------------------------------------------------------------------*/
+/**
+ * A mixin to add to components that get their initial state using the static getAsyncInitialState
+ * method.
+ */
+type AsyncInitialStateHandlerMixinType = { 
+	getInitalState: () => {[key: string]: any}; 
+	getSubRouteHandler: (props: ?[key: string]: any}) => ReactElement;
+};
+var AsyncInitialStateHandlerMixin: AsyncInitialStateHandlerMixinType = {
+	getInitialState(): {[key: string]: any} {
+		var displayName = this.constructor.displayName;
+		if(!this.props.initialState || !this.props.initialState[displayName]) return {};
+
+		return this.props.initialState[displayName];
+	},
+	getSubRouteHandler(props: {[key: string]: any} = {}): ReactElement {
+		var handlerProps = {};
+		for(var key in props) handlerProps[key] = props[key];
+		if(this.props.initialState) handlerProps.initialState = this.props.initialState;
+
+		return <RouteHandler {...handlerProps} />;
+	}
+};
+
+/*------------------------------------------------------------------------------------------------*/
+//	--- Exports ---
+/*------------------------------------------------------------------------------------------------*/
+module.exports = AsyncInitialStateHandlerMixin;
--- src/index.js
+++ src/index.js
@@ -5,3 +5,4 @@ module.exports.http = require('./http');
 module.exports.AsyncReact = require('./asyncReact');
 module.exports.AsyncRouter = require('./asyncReactRouter');
 module.exports.AsyncInitialStateMixin = require('./asyncInitialStateMixin');
+module.exports.AsyncInitialStateHandlerMixin = require('./asyncInitialStateHandlerMixin');
